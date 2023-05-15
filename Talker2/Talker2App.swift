607ed2848cd8f6ffdeee2133bd2dc3c8f61d29be
Cole Hershkowitz <jcoleh@gmail.com>
2023-05-15 16:41:14 -0400

    Initial Commit

---
+++ Talker2/Talker2App.swift
@@ -0,0 +1,17 @@
+//
+//  Talker2App.swift
+//  Talker2
+//
+//  Created by Cole Hershkowitz on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Talker2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Talker2UITests/Talker2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Talker2UITestsLaunchTests.swift
+//  Talker2UITests
+//
+//  Created by Cole Hershkowitz on 5/15/23.
+//
+
+import XCTest
+
+final class Talker2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
